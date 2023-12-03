3e51705b7af63463247098e893478d17a4b9af3d
Jaylyn Etienne <jaylynetienne3@gmail.com>
2023-12-03 00:20:45 -0600

    completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,29 +1,34 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
-
-
-
-
-
-
-
-
-
-
+function addItem(item) {
+  basket.push(item);
+  console.log(`item added,: ${item}`);
+  return true;
+}
+function listItems() {
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
+function empty() {
+  basket.length = 0;
+  console.log(`your basket has been emptied.`);
+}
 // DO NOT MODIFY
 // Used for automated testing
 try {
-    module.exports = {
-        basket: typeof basket !== 'undefined' ? basket : undefined,
-        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
-        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
-        maxItems: typeof maxItems !== 'undefined' ? maxItems : undefined,
-        empty: typeof empty !== 'undefined' ? empty : undefined,
-        isFull: typeof isFull !== 'undefined' ? isFull : undefined,
-        removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
-    };
-} catch(e) {
-    // Do nothing
+  module.exports = {
+    basket: typeof basket !== "undefined" ? basket : undefined,
+    addItem: typeof addItem !== "undefined" ? addItem : undefined,
+    listItems: typeof listItems !== "undefined" ? listItems : undefined,
+    maxItems: typeof maxItems !== "undefined" ? maxItems : undefined,
+    empty: typeof empty !== "undefined" ? empty : undefined,
+    isFull: typeof isFull !== "undefined" ? isFull : undefined,
+    removeItem: typeof removeItem !== "undefined" ? removeItem : undefined,
+  };
+} catch (e) {
+  // Do nothing
 }
